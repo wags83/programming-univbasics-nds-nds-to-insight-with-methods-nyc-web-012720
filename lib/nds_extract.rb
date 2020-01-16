c1c1dc9483b2cf7732a3f82f68a1ab99e67bb429
Greg Wagner <gpwagner@gmail.com>
2020-01-16 00:43:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ def gross_for_director(director_data)
 director_total = 0
 movie_index = 0
 while movie_index < director_data[:movies].length do
-  director_total += director_data[:movies][movie_index][:worldwide_grosses]
+  director_total += director_data[:movies][movie_index][:worldwide_gross]
   movie_index += 1
 end
 director_total
